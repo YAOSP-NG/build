05698c633da054e00e05d7b95e6f02fb0edab609
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-14 21:26:00 +0200

    [3/8] Add AppOps, PrivacyGuard and superuser (squashed)
    build embedded su binary
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -30,6 +30,13 @@ PRODUCT_PACKAGES := \
     EasterEgg \
     WallpaperCropper
 
+# Embed SuperUser
+SUPERUSER_EMBEDDED := true
+
+PRODUCT_PACKAGES += \
+    Superuser \
+    su
+
 PRODUCT_PACKAGES += \
     clatd \
     clatd.conf \
